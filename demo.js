c6fd48bb6d61b345cb329d57f37127cf4cd6de9e
rorywalsh <rorywalsh@ear.ie>
2021-04-12 16:21:13 +0100

    updatng demo.js

--- demo.js
+++ demo.js
@@ -14,22 +14,25 @@
         scene.fogColor = new BABYLON.Color3(0, 0, 0);
 
         var camera = scene.activeCamera;
-        var cameraBox = BABYLON.Mesh.CreateBox("CameraBox", 1, scene);
-        //cameraBox.isVisible = false;
-        //cameraBox.position = new BABYLON.Vector3(0, 0, 0);
-        //cameraBox.parent = camera;
+        var cameraBox = BABYLON.Mesh.CreateBox("CameraBox", .1, scene);
+        cameraBox.isVisible = false;
+        cameraBox.position = new BABYLON.Vector3(0, 0, 0);
+        cameraBox.parent = camera;
+
+        //let ambientVolume = 0;
+        //let ambientMusic = new BABYLON.Sound("ambientMusic", "./build/assets/Guitar1.wav", scene, null, { loop: true, autoplay: true, spatialSound: false, volume: ambientVolume });
 
 
-        // var gl = new BABYLON.GlowLayer("glow", scene, {
-        //     mainTextureSamples: 4
-        // });
-        // gl.customEmissiveColorSelector = function(mesh, subMesh, material, result) {
-        //     if (mesh.name === "Bandes lum") {
-        //         result.set(1, 1, 1, 1);
-        //     } else {
-        //         result.set(0, 0, 0, 0);
-        //     }
-        // }
+        var gl = new BABYLON.GlowLayer("glow", scene, {
+            mainTextureSamples: 4
+        });
+        gl.customEmissiveColorSelector = function(mesh, subMesh, material, result) {
+            if (mesh.name === "Bandes lum") {
+                result.set(1, 1, 1, 1);
+            } else {
+                result.set(0, 0, 0, 0);
+            }
+        }
 
         var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
         light.intensity = 0.7;
@@ -51,12 +54,12 @@
             }
         });
 
+
+
         //sample trigger sounds....
         let triggerSounds = [];
         triggerSounds.push(new OneShotCollisionSound({ file: "./build/assets/collision1.wav", x: 2, z: 0.5 }));
         triggerSounds.push(new OneShotCollisionSound({ file: "./build/assets/collision1.wav", x: 4.8, z: -3 }));
-
-
 
         var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
         var crosshair = new BABYLON.GUI.Image("crosshair", "./build/assets/crosshairWhite.png");
@@ -96,7 +99,7 @@
         }
 
         scene.onBeforeRenderObservable.add(() => {
-            textInfo.text = "Player's position X:" + (camera.position.x).toFixed(2) + " Z:" + (camera.position.z).toFixed(2) + "\nPicked Mesh: " + meshInCrosshair;
+            textInfo.text = "Player's position X:" + (camera.position.x).toFixed(2) + " Y:" + (camera.position.y).toFixed(2) + " Z:" + (camera.position.z).toFixed(2) + "\nPicked Mesh: " + meshInCrosshair;
             castRay();
             if (cameraBox) {
                 triggerSounds.forEach(oneShot => {
